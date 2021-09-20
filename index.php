b08d84dd76fed62a47017b0255eca9afeab65e92
rakhahibatullah <gobsspeed87@gmail.com>
2021-09-20 15:36:47 +0700

    isi coding

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-index
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Git - Project 01</title>
+</head>
+<body>
+    <P>Hello Semua, Saya sedang belajar Git<P> 
+</body>
+</html>
